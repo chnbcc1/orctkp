27843d61f0695ba81b41055dcf11b20b951cf978
chnbcc1 <cihan-fb@hotmail.com>
2026-09-15 03:45:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="ogrenci-takip-v7-2-12-smart-alerts-grade-refresh-private";
+const CACHE="ogrenci-takip-v7-3-0-smart-alerts-grade-refresh-private";
 const ASSETS=[
   "./",
   "./index.html",
